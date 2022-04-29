ae8f058f7c1c3b37b54ea4fa0c7f2d51fc2892a7
halimov09 <sunnatullohhalimov>
2022-04-29 10:32:26 +0500

    main

--- js/slidertext.js
+++ js/slidertext.js
@@ -1,12 +1,56 @@
-let slider = document.querySelector('.carusel__section');
-let left = document.querySelector('#left');
-let rigt = document.querySelector('#right');
 
+window.addEventListener('DOMContentLoaded', () =>{
+    const slides = document.querySelectorAll('.offer__slide'),
+          slideWrapper = document.querySelector('.offer__slider-wrapper'),
+          slideInner = document.querySelector('.offer__slider-inner'),
+          width = getComputedStyle(slideWrapper).width,
+          prev = document.querySelector('.offer__slider-prev'),
+          next = document.querySelector('.offer__slider-next'),
+          current = document.querySelector('#current');
 
-left.addEventListener('click', function(){
-    slider.style.transform = "translateX(-25%)";
-});
-rigt.addEventListener('click', function(){
-    slider.style.transform = "translateX(0%)";
-});
+    let slideIndex =100;
+    let offset = 0;
+
+    slideInner.style.width = 100 * slides.length + '%'
+
+    slideInner.style.display = 'flex';
+    slideInner.style.transition = '.6s ease'
+    slideWrapper.style.overflow = 'hidden';
+
+    slides.forEach(slide =>{
+        slide.style.width = width
+    })
+
+    next.addEventListener('click', () =>{
+        if(offset == (+width.slice(0, width.length - 3) * (slides.length - 1))){
+            offset = 0;
+        }else{
+            offset += +width.slice(0, width.length - 3)
+        }
+
+        slideInner.style.transform = `translateX(-${offset}px)`
+    })
+
+    prev.addEventListener('click', () =>{
+        if(offset == 0){
+            offset = (+width.slice(0, width.length - 3) * (slides.length - 1))
+        }else{
+            offset -= +width.slice(0, width.length - 3)
+        }
+
+        slideInner.style.transform = `translateX(-${offset}px)`
+
+        if(slideIndex == 1){
+            slideIndex = slides.length
+        }else{
+            slideIndex--
+        }
+
+        if(slides.length < 10){
+            current.textContent = `0${slideIndex}`
+        }else{
+            current.textContent = slideIndex
+        }
+    })
+})
 
